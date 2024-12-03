f4ed0667cbe07ca5b9c66cc8f73ef198808ce2ad
unknown <arthur.saussereau@hotmail.fr>
2024-12-04 00:58:42 +0100

    Added content in the JS file

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -1,3 +1,18 @@
-const menu = document.querySelector(".menuOffresDuMoment");
-const menuMenus = document.querySelector(".menuMenus");
-const menuBurgers = document.querySelector(".menuBurgers");
+
+window.addEventListener('load', function() {
+    const menus = document.querySelector("main").children;
+
+    const ongletItems = document.querySelector(".navMenus").children[0].children;
+    const onglet = document.querySelector(".navMenus").children[0];
+    const main = document.querySelector("main");
+
+    for(let i=0;i<ongletItems.length;i++) {
+        ongletItems[i].addEventListener('click', function() {
+            if(i<menus.length)
+            {
+                onglet.insertBefore(ongletItems[i],ongletItems[0]);
+                main.insertBefore(menus[i],main.children[0]);
+            }
+        });
+    }
+});
